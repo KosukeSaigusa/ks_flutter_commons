74a8b137f79f208d997d8192bfde30a67b025ef5
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-03-09 23:00:55 +0900

    feat: refactor widget

--- lib/src/widget/widget.dart
+++ lib/src/widget/widget.dart
@@ -1,5 +1,6 @@
-export './disable_controll_widget.dart';
+export './disable_control_widget.dart';
 export './firebase_auth/firebase_auth.dart';
 export './firestore/firestore.dart';
+export './image/image.dart';
 export './root_widget.dart';
 export './tap_to_unfocus_widget.dart';
