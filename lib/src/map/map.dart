1615ffa1fd60a9a688f28f18cf882d3063077e80
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2022-03-20 11:44:59 +0900

    feat: update map method

--- lib/src/map/map.dart
+++ lib/src/map/map.dart
@@ -1,11 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 /// 入力したマップの各キーの値を調べて、
-/// null のものはエントリーごと除外したしたマップを返す。
-Map<String, dynamic> removeNullValueMapEntries(Map<String, dynamic> map) {
+/// null のものはエントリーごと除外したマップを返す。
+Map<String, dynamic> removeNullValueMapEntries(Map<String, dynamic> m) {
   final nonNullValueMap = <String, dynamic>{};
-  map.entries.map((e) {
-    if (e.value != null) {
-      nonNullValueMap[e.key] = e.value;
+  m.forEach((key, dynamic value) {
+    if (value != null) {
+      nonNullValueMap.update(key, (dynamic value) => value);
     }
   });
+  return nonNullValueMap;
+}
+
+/// 入力したマップの各キーの値を調べて、
+/// null のものはエントリーごと除外した上で
+/// 'updatedAt': FieldValue.serverTimestamp() のキーバリューを付加したマップを返す。
+Map<String, dynamic> processMapToUpdateFirestoreDoc(
+  Map<String, dynamic> m, [
+  String updateAtFieldName = 'updatedAt',
+]) {
+  final nonNullValueMap = <String, dynamic>{};
+  m.forEach((key, dynamic value) {
+    nonNullValueMap[key] = value;
+  });
+  nonNullValueMap[updateAtFieldName] = FieldValue.serverTimestamp();
   return nonNullValueMap;
 }
